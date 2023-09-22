d7456c90ba1f06a079455ab8c418752db49a9895
wildarte <wildarte10@gmail.com>
2023-09-22 17:46:58 -0300

    responsivo home

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,30 @@
 $(document).ready(function(){
 
+    const btn_menu_mobile = document.querySelector('.btn_menu_mobile');
+    const btn_close_menu = document.querySelector('.btn_close_menu');
+    const nav_menu = document.querySelector('nav.menu');
+    const link_logo = document.querySelector('.link_logo');
+
+    $(".carousel_hero").owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        autoplay:true,
+        autoplayTimeout:2000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+
     $(".carousel_project").owlCarousel({
         loop:true,
         margin:0,
@@ -38,6 +63,16 @@ $(document).ready(function(){
                 items:3
             }
         }
+    });
+    
+    btn_menu_mobile.addEventListener('click', () =>{
+        nav_menu.classList.add('open_menu_mobile');
+        link_logo.classList.add('link_logo_fixed');
+    });
+
+    btn_close_menu.addEventListener('click', () =>{
+        nav_menu.classList.remove('open_menu_mobile');
+        link_logo.classList.remove('link_logo_fixed');
     });
 
 });
